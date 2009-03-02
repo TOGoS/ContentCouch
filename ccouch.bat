7e39c99d2b12cd024360cc37531ef3de94a7b829
TOGoS <TOGoS@.(none)>
2009-03-01 21:41:28 -0600

    Allow more parameters in ccouch.bat

--- ccouch.bat
+++ ccouch.bat
@@ -1,3 +1,45 @@
 @echo off
 
+rem Allow more than 9 arguments...
+set A1=%1
+shift
+set A2=%1
+shift
+set A3=%1
+shift
+set A4=%1
+shift
+set A5=%1
+shift
+set A6=%1
+shift
+set A7=%1
+shift
+set A8=%1
+shift
+set A9=%1
+shift
+set A10=%1
+shift
+set A11=%1
+shift
+set A12=%1
+shift
+set A13=%1
+shift
+set A14=%1
+shift
+set A15=%1
+shift
+set A16=%1
+shift
+set A17=%1
+shift
+set A18=%1
+shift
+set A19=%1
+shift
+set A20=%1
+shift
+
 java -cp %~dp0web\WEB-INF\classes contentcouch.app.ContentCouchCommand -repo:junk %~dp0junk-repo %1 %2 %3 %4 %5 %6 %7 %8 %9
