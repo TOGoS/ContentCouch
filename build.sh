7299a2db1c983f1abe7334d281b6ed0da60ec920
Dan Stevens <stevens@earthit16.(none)>
2009-03-19 06:53:35 -0500

    mkdir -p bin in build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 find src -name '*.java' >.src-files.lst
 OUTDIR=bin
+mkdir -p $OUTDIR
 javac -extdirs ext-lib -d "$OUTDIR" @.src-files.lst -target 1.4 -source 1.4
 cp src/contentcouch/repository/config-template.txt "$OUTDIR/contentcouch/repository/config-template.txt"
