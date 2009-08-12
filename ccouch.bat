1c014cca7e24ba29059d39520f317a8f1e9cc122
TOGoS <togos00@gmail.com>
2009-08-12 18:18:47 -0500

    Updated paths in shell scripts to include ext-lib/togos.mf-2009.08.07b.jar ... there should be a better way to keep these jar paths updated

--- ccouch.bat
+++ ccouch.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -cp %~dp0web\WEB-INF\classes contentcouch.app.ContentCouchCommand -repo %~dp0junk-repo %*
+java -cp %~dp0web\WEB-INF\classes;%dp0ext-lib\togos.mf-2009.08.07b.jar contentcouch.app.ContentCouchCommand -repo %~dp0junk-repo %*
