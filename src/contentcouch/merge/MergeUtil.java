34d02a02ce40f08f9b31008f82182e9d120df8ce
TOGoS <togos00@gmail.com>
2010-06-14 16:54:57 -0500

    Implemented findCommonAncestor better

--- src/contentcouch/merge/MergeUtil.java
+++ src/contentcouch/merge/MergeUtil.java
@@ -1,5 +1,12 @@
 package contentcouch.merge;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import contentcouch.contentaddressing.BitprintScheme;
 import contentcouch.misc.UriUtil;
 import contentcouch.misc.ValueUtil;
@@ -46,10 +53,79 @@ public class MergeUtil {
 		return null;
 	}
 	
-	public static String findCommonAncestor( String c1Urn, String c2Urn ) {
-		if( c1Urn.equals(c2Urn) ) return c1Urn;
+	protected static class AncestorFinder {
+		Map commits = new HashMap();
+		Map commitParents = new HashMap();
 		
-		// TODO: implement somehow
-		return null;
+		protected Commit getCommit( String urn ) {
+			if( commits.containsKey(urn) ) {
+				return (Commit)commits.get(urn);
+			}
+			Commit c = MergeUtil.getCommit(urn);
+			commits.put(urn,c);
+			return c;
+		}
+		
+		public Set getParentCommitUris( String commitUri ) {
+			Commit c = getCommit(commitUri);
+			if( c != null ) {
+				Set parentUris = new HashSet();
+				Object[] parents = c.getParents();
+				for( int i=parents.length-1; i>=0; --i ) {
+					if( parents[i] instanceof Ref ) {
+						parentUris.add( ((Ref)parents[i]).getTargetUri() );
+					}
+				}
+				return parentUris;
+			}
+			return Collections.EMPTY_SET;
+		}
+		
+		public Set getParentCommitUris( Set commitUris ) {
+			Set byGum = new HashSet();
+			for( Iterator i=commitUris.iterator(); i.hasNext(); ) {
+				byGum.addAll(getParentCommitUris((String)i.next()));
+			}
+			return byGum;
+		}
+		
+		public String findCommonAncestor( String c1Urn, String c2Urn ) {
+			if( c1Urn.equals(c2Urn) ) return c1Urn;
+			
+			Set newParents1 = getParentCommitUris(c1Urn);
+			Set newParents2 = getParentCommitUris(c2Urn);
+			Set ancestors1 = new HashSet();
+			ancestors1.add(c1Urn);
+			Set ancestors2 = new HashSet();
+			ancestors2.add(c2Urn);
+			
+			while( newParents1.size() > 0 || newParents2.size() > 0 ) {
+				if( newParents1.size() > 0 ) {
+					for( Iterator i=newParents1.iterator(); i.hasNext(); ) {
+						String curi = (String)i.next();
+						if( ancestors2.contains(curi) ) {
+							return curi;
+						}
+					}
+					ancestors1.addAll(newParents1);
+					newParents1 = getParentCommitUris(newParents1);
+				}
+				if( newParents2.size() > 0 ) {
+					for( Iterator i=newParents2.iterator(); i.hasNext(); ) {
+						String curi = (String)i.next();
+						if( ancestors1.contains(curi) ) {
+							return curi;
+						}
+					}
+					ancestors2.addAll(newParents2);
+					newParents2 = getParentCommitUris(newParents2);
+				}
+			}
+			return null;
+		}
+	}
+	
+	public static String findCommonAncestor( String c1Urn, String c2Urn ) {
+		return new AncestorFinder().findCommonAncestor(c1Urn, c2Urn);
 	}
 }
--- test/contentcouch/merge/MergeUtilTest.java
+++ test/contentcouch/merge/MergeUtilTest.java
@@ -17,7 +17,18 @@ import contentcouch.value.BaseRef;
 import contentcouch.value.Commit;
 import contentcouch.value.Ref;
 
-public class MergeUtilTest extends TestCase {
+public class MergeUtilTest extends TestCase {	
+	protected String SHA1A = "urn:sha1:AAAAFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1B = "urn:sha1:BBBBFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1C = "urn:sha1:CCCCFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1D = "urn:sha1:DDDDFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1E = "urn:sha1:EEEEFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1F = "urn:sha1:FFFFFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1G = "urn:sha1:GGGGFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1H = "urn:sha1:HHHHFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1I = "urn:sha1:IIIIFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	protected String SHA1J = "urn:sha1:JJJJFCQ6OUTZ2ETQPX2ZP3542WG4DY7V";
+	
 	MetaRepoConfig mrc;
 	RepoConfig testRepoConfig = new RepoConfig(RepoConfig.DISPOSITION_LOCAL, "x-memtemp:/test-repo/", "test-repo");
 	public void setUp() {
@@ -65,8 +76,8 @@ public class MergeUtilTest extends TestCase {
 	}
 
 	public void testUriEquivalence() {
-		assertTrue( MergeUtil.areUrisEquivalent( "urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V", "urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V" ) );
-		assertFalse( MergeUtil.areUrisEquivalent( "urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V", "urn:sha1:XXXXFCQ6OUTZ2ETQPX2ZP3542WG4DY7V" ) );
+		assertTrue( MergeUtil.areUrisEquivalent( SHA1A, SHA1A ) );
+		assertFalse( MergeUtil.areUrisEquivalent( SHA1A, SHA1B ) );
 		assertTrue( MergeUtil.areUrisEquivalent( "x-parse-rdf:urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V", "x-rdf-subject:urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V" ) );
 		assertNotNecessarilyTrue( MergeUtil.areUrisEquivalent( "x-rdf-subject:urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V", "urn:sha1:RSCTFCQ6OUTZ2ETQPX2ZP3542WG4DY7V" ) );
 	}
@@ -89,10 +100,10 @@ public class MergeUtilTest extends TestCase {
 			"urn:tree:tiger:TFUAXNIEPBE53C4KFYZVZNK3OU3E5MAVHSQPAEI" ) );
 	}
 	
-	public void xxx_testFindCommonParent() {
-		Ref parentRef = storeCommit(new BaseRef("urn:sha1:FOO"),null);
-		Ref c1Ref = storeCommit(new BaseRef("urn:sha1:BAR"),new Object[]{parentRef});
-		Ref c2Ref = storeCommit(new BaseRef("urn:sha1:BAZ"),new Object[]{parentRef});
+	public void testFindCommonParent() {
+		Ref parentRef = storeCommit(new BaseRef(SHA1A),null);
+		Ref c1Ref = storeCommit(new BaseRef(SHA1B),new Object[]{parentRef});
+		Ref c2Ref = storeCommit(new BaseRef(SHA1C),new Object[]{parentRef});
 		
 		assertFalse( parentRef.getTargetUri().equals(c1Ref.getTargetUri()));
 		assertFalse( parentRef.getTargetUri().equals(c2Ref.getTargetUri()));
@@ -101,4 +112,35 @@ public class MergeUtilTest extends TestCase {
 		assertEquals( parentRef.getTargetUri(), MergeUtil.findCommonAncestor(c1Ref.getTargetUri(), c2Ref.getTargetUri()) );
 	}
 
+	public void testFindCommonParentOfSameCommit() {
+		Ref parentRef = storeCommit(new BaseRef(SHA1A),null);
+		
+		assertEquals( parentRef.getTargetUri(), MergeUtil.findCommonAncestor(parentRef.getTargetUri(), parentRef.getTargetUri()) );
+	}
+
+	public void testFindCommonParentInLinearChain() {
+		Ref   rootRef = storeCommit(new BaseRef(SHA1D),new Object[]{});
+		Ref childARef = storeCommit(new BaseRef(SHA1A),new Object[]{rootRef});
+		Ref childBRef = storeCommit(new BaseRef(SHA1B),new Object[]{childARef});
+		Ref childCRef = storeCommit(new BaseRef(SHA1C),new Object[]{childBRef});
+		
+		assertEquals( rootRef.getTargetUri(), MergeUtil.findCommonAncestor(rootRef.getTargetUri(), childARef.getTargetUri()) );
+		assertEquals( rootRef.getTargetUri(), MergeUtil.findCommonAncestor(rootRef.getTargetUri(), childBRef.getTargetUri()) );
+		assertEquals( rootRef.getTargetUri(), MergeUtil.findCommonAncestor(rootRef.getTargetUri(), childCRef.getTargetUri()) );
+		
+		assertEquals( childBRef.getTargetUri(), MergeUtil.findCommonAncestor(childBRef.getTargetUri(), childCRef.getTargetUri()) );
+	}
+
+	public void testFindCommonParentInComplex() {
+		Ref aRef = storeCommit(new BaseRef(SHA1A),new Object[]{});
+		Ref bRef = storeCommit(new BaseRef(SHA1B),new Object[]{aRef});
+		Ref cRef = storeCommit(new BaseRef(SHA1C),new Object[]{aRef});
+		Ref dRef = storeCommit(new BaseRef(SHA1D),new Object[]{bRef});
+		Ref eRef = storeCommit(new BaseRef(SHA1E),new Object[]{bRef});
+		Ref fRef = storeCommit(new BaseRef(SHA1F),new Object[]{dRef,eRef});
+		
+		assertEquals( aRef.getTargetUri(), MergeUtil.findCommonAncestor(cRef.getTargetUri(), fRef.getTargetUri()) );
+		assertEquals( bRef.getTargetUri(), MergeUtil.findCommonAncestor(dRef.getTargetUri(), eRef.getTargetUri()) );
+		assertEquals( eRef.getTargetUri(), MergeUtil.findCommonAncestor(eRef.getTargetUri(), fRef.getTargetUri()) );
+	}
 }
