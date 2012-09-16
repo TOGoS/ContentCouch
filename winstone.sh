967d86b46a7f775ab15c4db59450ed3f27eae4b3
TOGoS <togos00@gmail.com>
2012-09-15 20:10:34 -0500

    Winstone.sh now execs java so that it will share the same PID, yo.

--- winstone.sh
+++ winstone.sh
@@ -1,6 +1,6 @@
 CCOUCH_PROJ_DIR=`dirname "$0"`
 
-java -jar "$CCOUCH_PROJ_DIR"/ext-lib/winstone-0.9.10.jar \
+exec java -jar "$CCOUCH_PROJ_DIR"/ext-lib/winstone-0.9.10.jar \
     --commonLibFolder="$CCOUCH_PROJ_DIR"/ext-lib \
     --useServletReloading=true \
     "$CCOUCH_PROJ_DIR"/web \
