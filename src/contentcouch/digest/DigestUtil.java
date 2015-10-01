2a27839130d2508a2ed00f30aa2b5b966da755b6
TOGoS <togos00@gmail.com>
2015-10-01 15:01:52 -0500

    When encountering IOException while digesting a file, iclude name of file in rethrown error message.

--- src/contentcouch/digest/DigestUtil.java
+++ src/contentcouch/digest/DigestUtil.java
@@ -34,7 +34,7 @@ public class DigestUtil {
 				fis.close();
 			}
 		} catch( IOException e ) {
-			throw new RuntimeException(e);
+			throw new RuntimeException("Failed to digest "+file, e);
 		}
 		return md.digest();
 	}
